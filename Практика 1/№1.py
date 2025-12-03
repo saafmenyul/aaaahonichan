34b233d0390a7606b429915019ba31bb0f0fc0be
saafmenyul <145987884+saafmenyul@users.noreply.github.com>
2025-12-03 16:43:31 +0500

    Update №1.py

--- Практика 1/№1.py
+++ Практика 1/№1.py
@@ -1,5 +1,7 @@
-tempcel = float(input())
-tempfar = (tempcel * 9/5) + 32
-tempkel = tempcel + 273.15
-print(round(tempfar, 2))
-print(round(tempkel, 2))
+temp_cel = float(input())
+
+temp_far = (temp_cel * 9 / 5) + 32
+temp_kel = temp_cel + 273.15
+
+print(round(temp_far, 2))
+print(round(temp_kel, 2))
