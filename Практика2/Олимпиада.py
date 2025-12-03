e257f8ea2bcdb4df5481f849b8e4f675e52dae47
saafmenyul <145987884+saafmenyul@users.noreply.github.com>
2025-12-03 16:46:56 +0500

    Update Олимпиада.py

--- Практика2/Олимпиада.py
+++ Практика2/Олимпиада.py
@@ -1,7 +1,10 @@
 def check_winners(scores, student_score):
-    scores.sort(reverse=True)
-    if scores[0] == student_score or scores[1] == student_score or scores[2] == student_score:
-        return "Вы в тройке победителей!"
-    else:
-        return "Вы не попали в тройку победителей."
+    # Сортируем по убыванию и берем топ-3
+    top_three = sorted(scores, reverse=True)[:3]
+    
+    if student_score in top_three:
+        return "Вы в тройке лучших!"
+    return "Вы не в тройке лучших."
+
+
 print(check_winners([20, 48, 52, 38, 36, 13, 7, 41, 34, 24, 5, 51, 9, 14, 28], 52))
