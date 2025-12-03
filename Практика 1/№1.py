1a47f697ee47b9b11625745c9e9ed5913c446b7b
saafmenyul <145987884+saafmenyul@users.noreply.github.com>
2025-12-03 16:44:21 +0500

    Update №1.py

--- Практика 1/№1.py
+++ Практика 1/№1.py
@@ -1,7 +1,12 @@
-temp_cel = float(input())
+# Получаем температуру в градусах Цельсия
+temp_cel = float(input("Введите температуру в °C: "))
 
+# Конвертируем в градусы Фаренгейта
 temp_far = (temp_cel * 9 / 5) + 32
+
+# Конвертируем в Кельвины
 temp_kel = temp_cel + 273.15
 
-print(round(temp_far, 2))
-print(round(temp_kel, 2))
+# Выводим результаты с округлением до двух знаков после запятой
+print(f"Температура в Фаренгейтах: {round(temp_far, 2)}°F")
+print(f"Температура в Кельвинах: {round(temp_kel, 2)}K")
